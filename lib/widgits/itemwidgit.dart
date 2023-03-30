648ae2ca406eea9e8cf98ce5c258ef22169d2034
Vansh-77 <vanshbhardwaj7306@gmail.com>
2023-03-30 18:09:39 +0530

    UI updated

--- lib/widgits/itemwidgit.dart
+++ lib/widgits/itemwidgit.dart
@@ -12,7 +12,7 @@ class ItemWidgit extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(12.0),
       child: Container(
-        height: 80,
+        height: 130,
         decoration: BoxDecoration(
             color: Colors.white,
             borderRadius: BorderRadius.circular(12),
@@ -27,29 +27,72 @@ class ItemWidgit extends StatelessWidget {
                 blurRadius: 0.5,
               )
             ]),
-        child: ListTile(
-          leading: Image.network(
-            item.image,
+        child: Row(children: [
+          SizedBox(
+            width: 120,
+            child: Image.network(
+              item.image,
+            ),
           ),
-          title: Text(item.name),
-          subtitle: Text(item.desc),
-          trailing: Container(
-              decoration: BoxDecoration(
-                color: Colors.deepOrange,
-                borderRadius: BorderRadius.circular(12),
-                boxShadow: const [
-                  BoxShadow(
-                    color: Colors.black,
-                    offset: Offset.zero,
-                    blurRadius: 1,
-                  )
-                ],
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(4.0),
-                child: Text("\$${item.price}"),
-              )),
-        ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 12.0, left: 10),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      item.name,
+                      style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      item.desc,
+                      style: const TextStyle(
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 0, right: 2),
+                      child: ButtonBar(
+                        alignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "\$${item.price}",
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 15,
+                            ),
+                          ),
+                          Container(
+                              padding: EdgeInsets.zero,
+                              decoration: BoxDecoration(
+                                color: Colors.deepPurple,
+                                borderRadius: BorderRadius.circular(12),
+                                boxShadow: const [
+                                  BoxShadow(
+                                    color: Colors.black,
+                                    offset: Offset.zero,
+                                    blurRadius: 1,
+                                  )
+                                ],
+                              ),
+                              child: const Padding(
+                                padding: EdgeInsets.all(5.0),
+                                child: Text(" Buy ",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w500)),
+                              ))
+                        ],
+                      ),
+                    )
+                  ]),
+            ),
+          ),
+        ]),
       ),
     );
   }
